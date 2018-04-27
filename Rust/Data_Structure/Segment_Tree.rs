bbda1f0c3a54b1038cd9f4c2f39a42ff2dc85756
ecasdqina <e1711@s.akashi.ac.jp>
2018-04-27 22:39:04 +0900

    I

--- Rust/Data_Structure/Segment_Tree.rs
+++ Rust/Data_Structure/Segment_Tree.rs
@@ -1,16 +1,13 @@
 struct SegmentTree{
-	seg: Vec<i64>,
 	sz: usize,
+	seg: Vec<i64>,
 }
 	
 impl SegmentTree {
 	fn new(n: usize) -> SegmentTree {
-		let mut sz: usize = 1;
-		while sz < n { sz *= 2; }
-		
 		SegmentTree {
-			seg: vec![0; 2 * sz],
-			sz : sz,
+			sz : n.next_power_of_two(),
+			seg: vec![0; 2 * n.next_power_of_two() - 1],
 		}
 	}
 	
@@ -23,15 +20,15 @@ impl SegmentTree {
 			self.seg[k] = self.seg[2 * k + 1] + self.seg[2 * k + 2];
 		}
 	}
-	
+
+	fn get(&self, a: usize, b: usize) -> i64 {
+		self._get(a, b, 0, 0, self.sz)
+	}	
+
 	fn _get(&self, a: usize, b: usize, k: usize, l: usize, r: usize) -> i64 {
 		if b <= l || r <= a { return 0; }
 		if a <= l && r <= b { return self.seg[k]; }
 		
 		self._get(a, b, 2 * k + 1, l, (l + r) / 2) + self._get(a, b, 2 * k + 2, (l + r) / 2, r)
-	}
-	
-	fn get(&self, a: usize, b: usize) -> i64 {
-		self._get(a, b, 0, 0, self.sz)
 	}
 }
